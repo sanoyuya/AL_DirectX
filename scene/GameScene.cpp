6c71074c5523bb63cd32cd71752ff2378c6c7b43
sano_yuya <k021g1256@g.neec.ac.jp>
2022-04-19 16:09:42 +0900

    バイオ歩き_定点カメラVer

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -52,29 +52,38 @@ void GameScene::Update() {
 	XMFLOAT3 move = {0, 0, 0};
 	XMFLOAT3 move2 = {0, 0, 0};
 
-	const float modelSpeed = 0.2f;
+	const float modelSpeed = 1.0f;
 	const float modelRotationSpeed = 0.02f;
 
-	if (input_->PushKey(DIK_UP)) {
-		move = {0, 0, modelSpeed};
-	} else if (input_->PushKey(DIK_DOWN)) {
-		move = {0, 0, -modelSpeed};
-	}
 	if (input_->PushKey(DIK_LEFT)) {
 		move2 = {0, -modelRotationSpeed, 0};
 	} else if (input_->PushKey(DIK_RIGHT)) {
 		move2 = {0, modelRotationSpeed, 0};
 	}
 
-	worldTransform_.translation_.x += move.x;
-	worldTransform_.translation_.y += move.y;
-	worldTransform_.translation_.z += move.z;
 	worldTransform_.rotation_.x += move2.x;
 	worldTransform_.rotation_.y += move2.y;
 	worldTransform_.rotation_.z += move2.z;
+
 	endWorldTransform_.rotation_.x += move2.x;
 	endWorldTransform_.rotation_.y += move2.y;
 	endWorldTransform_.rotation_.z += move2.z;
+
+	if (input_->PushKey(DIK_UP)) {
+		float x = sin(endWorldTransform_.rotation_.y) * modelSpeed;
+		float z = cos(endWorldTransform_.rotation_.y) * modelSpeed;
+
+		move = {x, 0, z};
+	} else if (input_->PushKey(DIK_DOWN)) {
+		float x = sin(endWorldTransform_.rotation_.y) * -modelSpeed;
+		float z = cos(endWorldTransform_.rotation_.y) * -modelSpeed;
+
+		move = {x, 0, z};
+	}
+
+	worldTransform_.translation_.x += move.x;
+	worldTransform_.translation_.y += move.y;
+	worldTransform_.translation_.z += move.z;
 
 	//行列の再計算
 	worldTransform_.UpdateMatrix();
