ec01a427269eda6b9cc23730a365f1dedb25e878
sano_yuya <k021g1256@g.neec.ac.jp>
2022-06-13 03:31:37 +0900

    でけた

--- Player.cpp
+++ Player.cpp
@@ -19,10 +19,22 @@ void Player::Initialize(Model* model, uint32_t textureHandle)
 	//ワールド変換の初期化
 	worldTransform_.Initialize();
 
+	//オブジェクト
+	worldTransform.translation_.x = 10;
+	worldTransform.translation_.z = 10;
+	worldTransform.Initialize();
+	worldTransform.translation_ = { 10,0,10 };
+	MatrixCalculation(worldTransform);
+	worldTransform.TransferMatrix();
+
 	//ビュープロジェクション初期化
 	viewProjection_.Initialize();
 	viewProjection_.eye.y = 20;
 
+	Vector3 camera(0, 0, 0);
+	camera = worldTransform_.translation_ - viewProjection_.eye;
+
+	vlength = Vector3Length(camera);
 }
 
 void Player::Update()
@@ -32,14 +44,20 @@ void Player::Update()
 	debugText_->SetPos(50, 50);
 	debugText_->Printf("translation_:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y, worldTransform_.translation_.z);
 	debugText_->SetPos(50, 70);
-	debugText_->Printf("mode:%d", mode);
+	debugText_->Printf("viewProjection_.eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf("mode:%d", mode_);
 }
 
-void Player::Move() {
+void Player::Move()
+{
 
 	Vector3 move(0, 0, 1);
 	Vector3 front(0, 0, 2);
 	Vector3 camera(0, 0, 0);
+	Vector3 cameraUp(0, 1, 0);
+	Vector3 cameraCross(0, 1, 0);
+
 
 	float rotationSpeed = 0.02f;
 	float moveSpeed = 0.05f;
@@ -47,46 +65,82 @@ void Player::Move() {
 	const float kEyeSpeed = 0.02f;
 
 	// 2πを超えたら0に戻す
-	viewAngle = fmodf(viewAngle, 2 * PI);
+	viewAngle_ = fmodf(viewAngle_, 2 * PI);
+	camera = worldTransform_.translation_ - viewProjection_.eye;
+	camera = Vector3Normalize(camera);
+
+	cameraCross = Vector3Cross(cameraUp, camera);
+	cameraCross = Vector3Normalize(cameraCross);
 
 	//視点ベクトルを計算(半径1の円周上の座標)
-	//viewProjection_.eye = { worldTransform_.translation_.x + sinf(viewAngle) * length, 20.0f,worldTransform_.translation_.x + cosf(viewAngle) * length };
 
-	if (mode == false) {
-		if (input_->PushKey(DIK_A)) {
+	if (mode_ == false)
+	{
+		if (input_->PushKey(DIK_A))
+		{
 			worldTransform_.rotation_.y -= rotationSpeed;
 		}
-		if (input_->PushKey(DIK_D)) {
+		if (input_->PushKey(DIK_D))
+		{
 			worldTransform_.rotation_.y += rotationSpeed;
 		}
 		// y軸回転行列演算
 		move.x = cos(worldTransform_.rotation_.y) * front.x + sin(worldTransform_.rotation_.y) * front.z;
 		move.z = -sin(worldTransform_.rotation_.y) * front.x + cos(worldTransform_.rotation_.y) * front.z;
 
-		if (input_->PushKey(DIK_W)) {
+		if (input_->PushKey(DIK_W))
+		{
 			worldTransform_.translation_.x += moveSpeed * move.x;
 			worldTransform_.translation_.z += moveSpeed * move.z;
 		}
-		if (input_->PushKey(DIK_S)) {
+		if (input_->PushKey(DIK_S))
+		{
 			worldTransform_.translation_.x -= moveSpeed * move.x;
 			worldTransform_.translation_.z -= moveSpeed * move.z;
 		}
 	}
-	else {
-		if (input_->PushKey(DIK_LEFT)) {
-			viewAngle -= kEyeSpeed;
+	else
+	{
+		viewProjection_.eye = { viewProjection_.target.x + sinf(viewAngle_) * length_, 20.0f,viewProjection_.target.z + cosf(viewAngle_) * length_ };
+
+		if (input_->PushKey(DIK_W))
+		{
+			camera = worldTransform_.translation_ - viewProjection_.eye;
+			camera = Vector3Normalize(camera);
+
+			worldTransform_.translation_.x += moveSpeed * camera.x;
+			worldTransform_.translation_.z += moveSpeed * camera.z;
 		}
-		if (input_->PushKey(DIK_RIGHT)) {
-			viewAngle += kEyeSpeed;
+		if (input_->PushKey(DIK_S))
+		{
+			camera = worldTransform_.translation_ - viewProjection_.eye;
+			camera = Vector3Normalize(camera);
+
+			worldTransform_.translation_.x -= moveSpeed * camera.x;
+			worldTransform_.translation_.z -= moveSpeed * camera.z;
+		}
+		if (input_->PushKey(DIK_D))
+		{
+			worldTransform_.translation_.x += moveSpeed * cameraCross.x;
+			worldTransform_.translation_.z += moveSpeed * cameraCross.z;
+		}
+		if (input_->PushKey(DIK_A))
+		{
+			worldTransform_.translation_.x -= moveSpeed * cameraCross.x;
+			worldTransform_.translation_.z -= moveSpeed * cameraCross.z;
+		}
+
+		if (input_->PushKey(DIK_LEFT))
+		{
+			viewAngle_ -= kEyeSpeed;
+		}
+
+		if (input_->PushKey(DIK_RIGHT))
+		{
+			viewAngle_ += kEyeSpeed;
 		}
 	}
-	
 
-	
-
-	
-
-	
 
 	worldTransform_.TransferMatrix();
 	viewProjection_.UpdateMatrix();
@@ -95,13 +149,17 @@ void Player::Move() {
 	MatrixCalculation(worldTransform_);
 }
 
-void Player::SelectMode() {
-	if (input_->TriggerKey(DIK_Q)) {
-		if (mode == false) {
-			mode = true;
+void Player::SelectMode()
+{
+	if (input_->TriggerKey(DIK_Q))
+	{
+		if (mode_ == false)
+		{
+			mode_ = true;
 		}
-		else {
-			mode = false;
+		else
+		{
+			mode_ = false;
 		}
 	}
 }
@@ -110,4 +168,5 @@ void Player::Draw()
 {
 	//3Dモデルの描画
 	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransform, viewProjection_, textureHandle_);
 }
--- Player.h
+++ Player.h
@@ -11,39 +11,41 @@
 class Player
 {
 public:
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	void Initialize(Model* model, uint32_t textureHandle);
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize(Model* model, uint32_t textureHandle);
 
-	/// <summary>
-	/// 毎フレーム処理
-	/// </summary>
-	void Update();
+    /// <summary>
+    /// 毎フレーム処理
+    /// </summary>
+    void Update();
 
-	void Move();
+    void Move();
 
-	void SelectMode();
+    void SelectMode();
 
-	/// <summary>
-	/// 描画
-	/// </summary>
-	void Draw();
+    /// <summary>
+    /// 描画
+    /// </summary>
+    void Draw();
 private:
-	Input* input_ = nullptr;
-	Audio* audio_ = nullptr;
-	DebugText* debugText_ = nullptr;
-	//ワールド変換データ
-	WorldTransform worldTransform_;
-	//ビュープロジェクション
-	ViewProjection viewProjection_;
-	//モデル
-	Model* model_ = nullptr;
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0u;
-	//Qキーで操作切り替え
-	bool mode = false;
-	float viewAngle = 3.141592654f;
-	float length = 30.0f;
-};
+    Input* input_ = nullptr;
+    Audio* audio_ = nullptr;
+    DebugText* debugText_ = nullptr;
+    //ワールド変換データ
+    WorldTransform worldTransform_;
+    WorldTransform worldTransform;
+    //ビュープロジェクション
+    ViewProjection viewProjection_;
+    //モデル
+    Model* model_ = nullptr;
+    //テクスチャハンドル
+    uint32_t textureHandle_ = 0u;
+    //Qキーで操作切り替え
+    bool mode_ = false;
+    float viewAngle_ = 3.141592654f;
+    float length_ = 30.0f;
 
+    float vlength;
+};
